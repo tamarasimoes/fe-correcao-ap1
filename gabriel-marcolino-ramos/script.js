d62ac1fe54657bf73fcaabe72fbca508070f23db
Gabriel Marcolino Ramos <gabrielmarcolinobh@gmail.com>
2024-04-16 15:47:58 -0300

    prova gabriel

--- gabriel-marcolino-ramos/script.js
+++ gabriel-marcolino-ramos/script.js
@@ -1,60 +1,71 @@
 // Questão 2
 
-function encontrarPrimos(inicio,fim) {
-    let sequencia = []
-    if (inicio != Number) {
-        console.log("Valor invalido")
-    } else if (fim != Number){
-        console.log("Valor invalido")
+function encontrarPrimos(inicio, fim) {
+    let sequencia = [];
+    if (typeof inicio !== 'number' || typeof fim !== 'number') {
+        console.log("Valor inválido");
     } else {
-        for (i=inicio, i < fim, i++) { // erro de sintaxe
-            
+        for (let i = inicio; i <= fim; i++) {
+            let primo = true;
+            for (let j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j === 0) {
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo && i > 1) {
+                sequencia.push(i);
+            }
         }
     }
-
+    return sequencia;
 }
 
-console.log(encontrarPrimos(5, 50))
+console.log(encontrarPrimos(5, 50));
 
 
 // Questão 3
 
-var biblioteca = [
-    {titulo:'livro de python',autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'},
-    {titulo: 'livro de python',autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'},
-    {titulo: 'livro de python',autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'},
-]
+var biblioteca = {
+    livros: [
+        {titulo:'livro de python', autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'},
+        {titulo: 'livro de python', autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'},
+        {titulo: 'livro de python', autor: 'Sabrina', genero: 'edu', anoPublicacao: '2020'}
+    ]
+};
 
-const livro = {
+var novoLivro = {
     titulo: 'java',
     autor: "tamara",
     genero: "edu",
     anoPublicacao: "2010"
-}
+};
 
-biblioteca.push({livro})
+biblioteca.livros.push(novoLivro);
 
-function listarLivros(biblioteca){
-    for (i=0,i < biblioteca.length, i++){ // erro de sintaxe
-        console.log(biblioteca[i]['titulo'])
-        
+function listarLivros(biblioteca) {
+    for (let i = 0; i < biblioteca.livros.length; i++) {
+        console.log(biblioteca.livros[i]['titulo']);
     }
 }
 
-console.log(listarLivros(biblioteca))
+listarLivros(biblioteca);
+
+
+
 
 // Questão 4
 
 function contarVogais(string) {
-    var Vogais = [A,E,I,O,U,a,e,i,o,u];
-    let nVogais;
-    for (i = 0, i < string.lenght, i++) { // erro de sintaxe
-        if (string[i] in Vogais) { 
-            nVogais += 1
-        } 
+    var vogais = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+    let nVogais = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (vogais.includes(string[i])) {
+            nVogais++;
+        }
     }
-    return nVogais
-    
+    return nVogais;
 }
 
-console.log("O número de vogais dessa palavra é"+ nVogais)
+let palavra = "exemplo";
+console.log("O número de vogais nesta palavra é: " + contarVogais(palavra));
